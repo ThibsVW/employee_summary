de0a54e647dc3f928271fb0513caff7bf5a1b9a0
thibs <thibault.vanweydeveld@hotmail.com>
2020-07-15 07:00:42 +0800

    questions added

--- Develop/app.js
+++ Develop/app.js
@@ -11,6 +11,87 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+const internQuestions = [
+    {
+        type: "input",
+        message: "Enter the intern's name: ",
+        name: "internName"
+    },
+    {
+        type: "input",
+        message: "Enter the intern's ID number: ",
+        name: "internId"
+    },
+    {
+        type: "input",
+        message: "Enter the intern's E-mail address: ",
+        name: "internEmail"
+    },
+    {
+        type: "input",
+        message: "Enter your School: ",
+        name: "internSchool"
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "Enter the Engineer's name: ",
+        name: "engineerName"
+    },
+    {
+        type: "input",
+        message: "Enter the Engineer's ID number: ",
+        name: "engineerId"
+    },
+    {
+        type: "input",
+        message: "Enter the Engineer's E-mail address: ",
+        name: "engineerEmail"
+    },
+    {
+        type: "input",
+        message: "Enter your Github Account: ",
+        name: "engineerGithub"
+    }
+]
+
+const introQuestions = [
+    {
+        type: "input",
+        message: "Enter the Manager's Name: ",
+        name: "managerName"
+    },
+    {
+        type: "input",
+        message: "Enter Manager's ID number: ",
+        name: "managerId"
+    },
+    {
+        type: "input",
+        message: "Enter the Manager's E-mail: ",
+        name: "managerEmail"
+    },
+    {
+        type: "input",
+        message: "Enter the Manager's Office Number: ",
+        name: "managerNumber"
+    },
+    {
+        type: "input",
+        message: "How many Engineers are on the team? ",
+        name: "engineerAmount"
+    },
+    {
+        type: "input",
+        message: "how many Interns are on the team? ",
+        name: "internAmount"
+    }
+]
+
+init();
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
